f9e265534aa05f7a92047fe50ee74b4f4479cd5d
Ekaterina Shishkova <asinonyx@gmail.com>
2018-11-05 16:01:25 +0300

    adedd text preprocessing

--- get_hashtag.py
+++ get_hashtag.py
@@ -1,17 +1,33 @@
+import re
 import instaparser
-import json
 from instaparser.agents import Agent
 from instaparser.entities import Tag, Media
+
+def preprocess_text(text):
+    text = text.lower().replace("ё", "е")
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', text)
+    text = re.sub('@[^\s]+', 'USER', text)
+    text = re.sub('#[^\s]+', 'TAG', text)
+    text = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', text)
+    text = re.sub('[a-zA-Z]+', ' ', text)
+    text = re.sub(' +', ' ', text)
+    return text.strip()
+
 agent = Agent()
-def get_comments_by_tag(tag):
-	tag = Tag("tag")
-	medias = agent.get_media(obj=tag, count = 10) #get tuple with post identifiers and tag identifier
+
+def get_comments_by_tag(word, numberOfPost):
+	tag = Tag(word)
+	medias = agent.get_media(obj=tag, count = numberOfPost ) #get tuple with post identifiers and tag identifier
 	for media in medias[0]:
 		post = agent.update(obj=Media(media))#get post with comments
 		comments=""
 		counter =0
-		for text in post["edge_media_to_comment"]["edges"]:
-			comments += ((text["node"]["text"]) +"\n") #create str from comments
-			with open("{}.txt".format(media), "w", encoding = "utf-8" ) as f:
-				f.write(comments) 
-get_comments_by_tag("copper")
+		with open("{}.txt".format(media), "w", encoding = "utf-8" ) as f:
+			for t in post["edge_media_to_comment"]["edges"]:
+			    comment = str(t["node"]["text"]) #create str from 
+			    comment = preprocess_text(comment)
+			    print(comment)
+			    comments += comment
+			    f.write(comments) 
+
+get_comments_by_tag("гальванопластика", 5)
