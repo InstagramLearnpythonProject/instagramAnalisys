1156943508242d5de65f29a674f475f05d36dd43
Ekaterina Shishkova <asinonyx@gmail.com>
2018-11-05 17:48:56 +0300

    Empty post is not saving. Function returns number of post with comments

--- get_hashtag.py
+++ get_hashtag.py
@@ -1,4 +1,5 @@
 import re
+import os
 import instaparser
 from instaparser.agents import Agent
 from instaparser.entities import Tag, Media
@@ -16,18 +17,25 @@ def preprocess_text(text):
 agent = Agent()
 
 def get_comments_by_tag(word, numberOfPost):
-	tag = Tag(word)
-	medias = agent.get_media(obj=tag, count = numberOfPost ) #get tuple with post identifiers and tag identifier
-	for media in medias[0]:
-		post = agent.update(obj=Media(media))#get post with comments
-		comments=""
-		counter =0
-		with open("{}.txt".format(media), "w", encoding = "utf-8" ) as f:
-			for t in post["edge_media_to_comment"]["edges"]:
-			    comment = str(t["node"]["text"]) #create str from 
-			    comment = preprocess_text(comment)
-			    print(comment)
-			    comments += comment
-			    f.write(comments) 
+    try:
+        os.mkdir("{}".format(word))
+    except FileExistsError:
+    	pass
+    tag = Tag(word)
+    medias = agent.get_media(obj=tag, count = numberOfPost ) #get tuple with post identifiers and tag identifier
+    counter = 0
+    for media in medias[0]:
+        post = agent.update(obj=Media(media))#get post with comments
+        if post["edge_media_to_comment"]["edges"] != []:
+            counter += 1
+            comments = ""
+            with open("{}/{}.txt".format(word, media), "w", encoding = "utf-8", ) as f:
+                for t in post["edge_media_to_comment"]["edges"]:
+                    comment = str(t["node"]["text"]) #create str from 
+                    comment = preprocess_text(comment)
+                    comments = (comment + "\n")
+                    f.write(comments)
+    return counter
 
 get_comments_by_tag("гальванопластика", 5)
+print(get_comments_by_tag("гальванопластика", 5))
